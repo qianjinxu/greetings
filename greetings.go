93350409ccd6c2d2673ff80eb207cece4dbebc14
Qianjin Xu <xuqianjinchn@gmail.com>
2023-12-22 15:49:17 +0800

    Add Slice

--- greetings.go
+++ greetings.go
@@ -9,12 +9,25 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
-		return "", errors.New("Empty Name")
+		return name, errors.New("Empty Name")
 	}
-	message := fmt.Sprintf("Hello, %v. Welcome!", name)
+
+	message := fmt.Sprintf(randomFormat(), name)
+
 	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
